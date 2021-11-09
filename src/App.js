bb56e6f8a332e23f83c8fe4c9195febb8a43eee2
NikolaJ <jovanovicn369@gmail.com>
2021-11-09 19:02:53 +0100

    1st task

--- src/App.js
+++ src/App.js
@@ -2,23 +2,43 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  {/* • U komponentu postaviti niz objekata koji predstavlja niz od 5 proizvoljnih životinja. 
+  Svaka životinja treba da ima:
+  - vrstu
+  - ime
+  - datum rođenja ( new Date() )
+  • `AnimalList` treba da prikaže sve životinje dodate u listu. Prikazati sva tri polja u tabeli. */}
+
+  const animalArray = [{ type: 'mammal', name: 'Elephant', dateOfBirth: new Date().toLocaleDateString() },
+  { type: 'mammal', name: 'Lion', dateOfBirth: new Date().toLocaleDateString() },
+  { type: 'bird', name: 'Pigeon', dateOfBirth: new Date().toLocaleDateString() },
+  { type: 'insect', name: 'Ant', dateOfBirth: new Date().toLocaleDateString() },
+  { type: 'reptile', name: 'Turtle', dateOfBirth: new Date().toLocaleDateString() },
+  ]
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <table name='animal-table'>
+        <thead>
+          <tr>
+            <th>Type</th>
+            <th>Name</th>
+            <th>Date of Birth</th> </tr>
+        </thead>
+        <tbody>
+          {animalArray.map((animal, index) => (
+            <tr>
+              <td key={animal.type + index}>{animal.type}</td>
+              <td key={animal.name + index}>{animal.name}</td>
+              <td key={animal.dateOfBirth + index}>{animal.dateOfBirth}</td>
+            </tr>
+
+          ))}
+        </tbody>
+
+      </table>
+    </div >
   );
 }
 
