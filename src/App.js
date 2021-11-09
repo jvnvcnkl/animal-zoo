e1ae5ede7b0e13bb529e16f3109bb4d37b4b6a63
NikolaJ <jovanovicn369@gmail.com>
2021-11-09 20:17:42 +0100

    3rd task

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+
+
+const ANIMALS = [{ type: 'mammal', name: 'Elephant', dateOfBirth: new Date().toLocaleDateString() },
+{ type: 'mammal', name: 'Lion', dateOfBirth: new Date().toLocaleDateString() },
+{ type: 'bird', name: 'Pigeon', dateOfBirth: new Date().toLocaleDateString() },
+{ type: 'insect', name: 'Ant', dateOfBirth: new Date().toLocaleDateString() },
+{ type: 'reptile', name: 'Turtle', dateOfBirth: new Date().toLocaleDateString() },
+{ type: 'insect', name: 'Bee' },
+]
+
 
 function App() {
   {/* • U komponentu postaviti niz objekata koji predstavlja niz od 5 proizvoljnih životinja. 
@@ -9,13 +20,17 @@ function App() {
   - datum rođenja ( new Date() )
   • `AnimalList` treba da prikaže sve životinje dodate u listu. Prikazati sva tri polja u tabeli. */}
 
-  const animalArray = [{ type: 'mammal', name: 'Elephant', dateOfBirth: new Date().toLocaleDateString() },
-  { type: 'mammal', name: 'Lion', dateOfBirth: new Date().toLocaleDateString() },
-  { type: 'bird', name: 'Pigeon', dateOfBirth: new Date().toLocaleDateString() },
-  { type: 'insect', name: 'Ant', dateOfBirth: new Date().toLocaleDateString() },
-  { type: 'reptile', name: 'Turtle', dateOfBirth: new Date().toLocaleDateString() },
-  ]
 
+
+
+  const [animalArray, setAnimal] = React.useState(ANIMALS)
+
+
+
+  const handleClick = (animalIndex) => {
+
+    setAnimal(animalArray.filter((animal, index) => animalIndex !== index))
+  }
 
   return (
     <div className="App">
@@ -30,8 +45,10 @@ function App() {
           {animalArray.map((animal, index) => (
             <tr>
               <td key={animal.type + index}>{animal.type}</td>
+
               <td key={animal.name + index}>{animal.name}</td>
-              <td key={animal.dateOfBirth + index}>{animal.dateOfBirth}</td>
+              <td key={animal.dateOfBirth + index}>{animal.dateOfBirth ? animal.dateOfBirth : 'Unkown'}</td>
+              <button onClick={e => handleClick(index)}> Remove</button>
             </tr>
 
           ))}
